af97dd53cfa5a01a7ddb50295c1a5a112058fbe7
julien <j.paput@gmail.com>
2019-08-04 17:06:43 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+    socket.on('join', function(userNickname) {
+
+        console.log(userNickname +" : has joined the chat "  )
+        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ")
+
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        socket.broadcast.emit( "userdisconnect" ,' user has left')
+
+    });
+
+});
+
+http.listen(8080, function(){
+    console.log('Server is now listening !');
+});
