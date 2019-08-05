d6d7cbcf9279a4352f9a2e4ab3fa0a85ccd77108
julien <j.paput@gmail.com>
2019-08-05 09:29:44 +0200

    change port listen

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
     .use((req, res) => res.sendFile(INDEX) )
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .listen(process.env.PORT || 5000, () => console.log(`UGC server now Listening`));
 
 const io = socketIO(server);
 
