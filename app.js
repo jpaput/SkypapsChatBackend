688ef1031f4b54b6ba89a60d545fb57be270382e
julien <j.paput@gmail.com>
2019-08-06 10:17:43 +0200

    amélioration

--- app.js
+++ app.js
@@ -8,30 +8,35 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
     .use((req, res) => res.sendFile(INDEX) )
-    .listen(process.env.PORT || 5000, () => console.log(`UGC server now Listening`));
+    .listen(process.env.PORT || 5002, () => console.log(`UGC server now Listening`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-    console.log('Client connected');
+
+    socket.emit('serverMessage', 'Welcome, you are now connected to chat.');
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected')
+        console.log(socket.userNickname + " disconnected");
+        socket.broadcast.emit('serverMessage', socket.userNickname + " leave the chat.");
+
     });
 
     socket.on('join', userNickname => {
+        socket.userNickname = userNickname;
 
-        console.log(userNickname +" : has joined the chat "  )
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ")
+        console.log(socket.userNickname + " connected");
+        socket.broadcast.emit('serverMessage',socket.userNickname + " join the chat.");
 
     });
 
-    socket.on('chat message', msg => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('newMessage', messageContent => {
+
+        console.log(socket.userNickname + " : " + messageContent);
+        socket.broadcast.emit('userMessageSent', {message : messageContent, senderNickname : socket.userNickname});
     });
 });
 
 setInterval(() => {
-    io.emit('time', new Date().toTimeString())
+    io.emit('time', new Date().toTimeString());
 }, 1000);
